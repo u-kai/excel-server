b15816a1b781f123f0c5add1b1d30679e6c53a46
u-kai@win <dummy@example.com>
2023-01-03 03:44:11 +0000

    add AlfabetIndex.to_number
    add and clear test

--- lib/excel_index.py
+++ lib/excel_index.py
@@ -1,6 +1,36 @@
+
+
 class ExcelIndex:
     def __init__(self,index:str):
         self.index = index 
 
     def value(self)->str:
         return self.index
+
+    def right(self):
+        return ExcelIndex("B1")
+
+
+class AlfabetIndex:
+    ALFABET_NUM = 26
+    ALFABETS = {"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26,}
+    def __init__(self,alfabets):
+        self.alfabets = alfabets
+
+
+    def to_number(self):
+        number = 0
+        splited = list(self.alfabets)
+        splited.reverse()
+        for digit,alfabet in enumerate(splited):
+            alfabet_num = AlfabetIndex.ALFABETS[alfabet]
+            digit_effect = AlfabetIndex.ALFABET_NUM**digit
+            this_digit_alfabet_num = alfabet_num * digit_effect
+            number += this_digit_alfabet_num
+        return number
+
+    def value(self):
+        return self.alfabets
+
+    def next(self):
+        return AlfabetIndex("B")
--- lib/excel_index_test.py
+++ lib/excel_index_test.py
@@ -1,6 +1,37 @@
-from excel_index import ExcelIndex
+from excel_index import AlfabetIndex, ExcelIndex
 
 
 def test_excel_index():
     index = ExcelIndex("A1")
     assert index.value() =="A1"
+
+def test_right_index():
+    index = ExcelIndex("A1")
+    right = index.right()
+    assert right.value() == "B1"
+    # right_right = right.right()
+    # assert right.value() == "C1"
+    
+
+def test_alfabet_index():
+    a = AlfabetIndex("A")
+    assert a.value() == "A"
+    b = AlfabetIndex("B")
+    assert b.value() == "B"
+    
+    
+def test_to_number():
+    a = AlfabetIndex("A")
+    assert a.to_number() == 1
+    a = AlfabetIndex("B")
+    assert a.to_number() == 2
+    aa = AlfabetIndex("AA")
+    assert aa.to_number() == 27
+    aa = AlfabetIndex("AB")
+    assert aa.to_number() == 28
+    
+# def test_next_alfabet():
+#     a = AlfabetIndex("A")
+#     assert a.next().value()  == "B"
+#     a = AlfabetIndex("AA")
+#     assert a.next().value()  == "AB"

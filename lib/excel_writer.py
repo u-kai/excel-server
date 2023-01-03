f7d20d154e6fb7256bc745285b38357c01bf18a2
u-kai@win <dummy@example.com>
2023-01-03 06:04:36 +0000

    add write_block_cell
    write_block_cellを使うことで指定したcellから自動的に値を順々に挿入してくれる

--- lib/excel_writer.py
+++ lib/excel_writer.py
@@ -1,3 +1,4 @@
+from excel_index import ExcelIndex
 from openpyxl import load_workbook
 
 
@@ -6,8 +7,20 @@ class ExcelWriter:
         self.wb = load_workbook(template_sheet_path)
         pass
 
-    def write_cell(self, filepath, sheet_name, cell_index, content):
+    def write_block_cell(self, sheet_name, start_index, contents):
+        start_index = ExcelIndex(start_index)
+        index = start_index
+        for row in contents:
+            for content in row:
+                self.write_cell(sheet_name, index.value(), content) 
+                index = index.right()
+            start_index = start_index.under()  
+            index = start_index
+
+    def write_cell(self, sheet_name, cell_index, content):
         sheet = self.wb[sheet_name]
         sheet[cell_index] = content
-        self.wb.save(filepath)
         return
+
+    def save(self, filepath):
+        self.wb.save(filepath)
--- lib/excel_writer_test.py
+++ lib/excel_writer_test.py
@@ -7,11 +7,48 @@ from excel_writer import ExcelWriter
 def test_excel_writer():
     write_file = "write-test.xlsx"
     writer = ExcelWriter("./lib/template.xlsx")
-    writer.write_cell(write_file, "Sheet1", "A1", "hello")
+    writer.write_cell("Sheet1", "A1", "hello")
+    writer.save(write_file)
     reader = ExcelReader()
     assert reader.read_cell("write-test.xlsx", "Sheet1", "A1") == "hello"
     remove(write_file)
-    
+
+
+def test_excel_writer_write_block():
+    writer = ExcelWriter("./lib/template.xlsx")
+    write_content = [
+        [
+            "A", "B", "C"
+        ],
+        [
+            "D", "E", "F", "G"
+        ],
+        [
+            "H", "I", "J", "K", "L"
+        ],
+    ]
+    writer.write_block_cell("Sheet1", "A1", write_content)
+    write_file = "write-test.xlsx"
+    writer.save(write_file)
+    reader = ExcelReader()
+    contents = reader.read_block_cell("write-test.xlsx", "Sheet1", "A1", "E3")
+    print(contents)
+    assert contents == [
+        [
+            "A", "B", "C", None, None
+        ],
+
+        [
+            "D", "E", "F", "G", None
+        ],
+
+        [
+            "H", "I", "J", "K", "L"
+        ],
+    ]
+
+    remove(write_file)
+
 
 def remove_file(filepath):
     remove(filepath)

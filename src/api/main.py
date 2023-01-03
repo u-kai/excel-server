9eec12ad80d9a63f5f1405bc9406c1c14f412810
u-kai@win <dummy@example.com>
2023-01-03 08:30:11 +0000

    add read-cell
    read-cellをpostで実装したが、パラメータが少なく、意味的にはgetなのでgetメソッドでもいいかも

--- src/api/main.py
+++ src/api/main.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 sys.path.append("../lib")
 
 
+from excel_reader import ExcelReader
 from excel_writer import ExcelWriter
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -11,14 +12,16 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+
+
+TEMPLATE_XLSX = "../lib/template.xlsx"
+
+
 class WriteContents(BaseModel):
     filename: str
     sheet: Optional[str] = "Sheet1"
     contents: List[List[str]]
     start_cell: Optional[str] = "A1"
-
-
-TEMPLATE_XLSX = "../lib/template.xlsx"
 
 
 @app.post("/write-contents")
@@ -29,3 +32,36 @@ async def write_contents(contents: WriteContents):
     )
     writer.save(contents.filename)
     return {"message": f"{contents.filename} is writed success"}
+
+
+class WriteContentsFromTemplate(BaseModel):
+    template:str
+    filename: str 
+    sheet: Optional[str] = "Sheet1"
+    contents: List[List[str]]
+    start_cell: Optional[str] = "A1"
+
+
+@app.post("/from-template")
+async def write_contents_from_template(contents:WriteContentsFromTemplate):
+    writer = ExcelWriter(contents.template)
+    writer.write_block_cell(contents.sheet,contents.start_cell,contents.contents)
+    writer.save(contents.filename)
+    return {"message":f"{contents.filename} is writed success"}
+
+
+class ReadContents(BaseModel):
+    filename:str
+    sheet: Optional[str] = "Sheet1"
+    cell:str
+
+reader = ExcelReader()
+
+
+@app.post("/read-cell")
+async def read_cell(contents:ReadContents):
+    cell = reader.read_cell(contents.filename,contents.sheet,contents.cell)
+    if cell is None:
+        return {"index":contents.cell,"value":""}
+
+    return {"index":contents.cell,"value":cell}

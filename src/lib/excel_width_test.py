d8dd4fc4d140851671e5f550dbef110ba3bab5ca
u-kai <dummy@example.com>
2023-01-05 11:41:25 +0000

    add calcurate_excel_width
    calcurate_excel_widthによって、幅の自動計算が可能.改行文字が出現したら改行文字で区切って計算を行うようにした

--- src/lib/excel_width_test.py
+++
@@ -1,9 +0,0 @@
-def test_calcrate_excel_width():
-    column_datas = [
-        "abcd",
-        "abcde",
-        "abcdef",
-        "g",
-    ]
-    char_width = 1.0
-    #assert calcrate_excel_width(column_datas,char_width) == 6.0

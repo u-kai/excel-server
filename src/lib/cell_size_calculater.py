00c342d96af278328c9a03d101ac6d5959f901a1
u-kai <dummy@example.com>
2023-01-05 12:27:38 +0000

    add calculate_heights
    難しかったのでテーブルデータを転置
    してから計算を行った

--- src/lib/cell_size_calculater.py
+++ src/lib/cell_size_calculater.py
@@ -9,12 +9,29 @@ class CellSizeCalculator:
             result.append(calculate_excel_height(row,self.char_height))
         return result
 
-#    def calculate_widths(self,table_data):
-#        result = []
-#        for column_data in table_data :
-#           result.append(calculate_excel_width(column_data,self.char_width))
-#        return result
+    def calculate_widths(self,table_data):
+        result = []
+        reversed = reverse(table_data)
+        print(reversed)
+        for column in reversed:
+            result.append(calculate_excel_width(column,self.char_width))
+        return result
 
+
+
+
+def reverse(table_data):
+    result = []
+    row_lengths = [len(row) for row in table_data]
+    max_row_length = max(row_lengths)
+    for i in range(max_row_length):
+        tmp = []
+        for row in table_data :
+            if i >= len(row):
+                continue
+            tmp.append(row[i])
+        result.append(tmp)
+    return result
 
 DEFAULT_CHAR_HEIGHT = 2.08
 def calculate_excel_height(row_datas,char_height=DEFAULT_CHAR_HEIGHT):
--- src/lib/cell_size_calculater_test.py
+++ src/lib/cell_size_calculater_test.py
@@ -14,5 +14,21 @@ def test_cell_size_calculater():
     heights = calculater.calculate_heights(datas)
     assert heights == [2*char_height,3*char_height]
     
-#    widths= calculater.calculate_widths(datas)
-#    assert widths == [3*char_width,4*char_width,5*char_width]
+    widths= calculater.calculate_widths(datas)
+    assert widths == [3*char_width,4*char_width,5*char_width]
+
+def test_cell_size_calculater_case_irregular():
+    char_width = 1.0
+    char_height = 1.0
+    
+    calculater = CellSizeCalculator(char_width,char_height)
+    
+    datas = [
+        ["A"+"\n"+"Z","BC","DEF"],
+        ["ABC","DEFG","HIJKL"+"\n"+"MN"+"\n"+"OPQR","STU"],
+    ]
+    heights = calculater.calculate_heights(datas)
+    assert heights == [2*char_height,3*char_height]
+    
+    widths= calculater.calculate_widths(datas)
+    assert widths == [3*char_width,4*char_width,5*char_width,3*char_width]
